098787ffe090e4c2d57dfbdbdf89d563219ca5d1
Khaled Ghourani <Khaledghourani4@gmail.com>
2025-10-19 17:43:58 +0300

    fixes

--- pm2.config.js
+++ pm2.config.js
@@ -3,7 +3,7 @@ module.exports = {
       {
         name: "transgate-backend",
         script: "./app", // Replace with your built Go binary name
-        cwd: "/www/wwwroot/transgatelb.linksbridge.top/backend-go/cmd/",
+        cwd: "/www/wwwroot/transgatelb.linksbridge.top/backend-go/cmd",
         exec_mode: "fork",
         autorestart: true
       },
